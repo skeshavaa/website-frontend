c9c3c2ed4e9e84e4ba257cc05337f56bf6d4a0b9
skeshavaa <s.keshavaa@gmail.com>
2020-10-04 00:38:00 -0400

    almost finished about me

--- src/components/about-me-info.jsx
+++ src/components/about-me-info.jsx
@@ -6,6 +6,17 @@ const AboutInfo = () => {
         <Wrapper>
             <Header>About Me</Header>
             <Divider />
+            <Info>
+                Hey. I'm a second year <InfoHighlight>Computer Science </InfoHighlight>
+                student at the <InfoHighlight>University of Toronto</InfoHighlight>
+                <br/><br/>
+                <InfoHighlight>I like making things. </InfoHighlight>Whether it's a web 
+                application or mobile app, <InfoHighlight> I'm gonna do it.</InfoHighlight>
+                <br/><br/>
+                <InfoHighlight>Check out my work below!</InfoHighlight>
+                <br/><br/>
+                <InfoHighlight>PS: Recruiters HIT ME UP :)</InfoHighlight>
+            </Info>
         </Wrapper>
     )
 }
@@ -14,6 +25,7 @@ export default AboutInfo
 
 const Wrapper = styled.div`
     width: 50%;
+    height: auto;
 `
 
 const Header = styled.h1`
@@ -26,4 +38,19 @@ const Divider = styled.div`
     height: 3px;
     width: 40px;
     background-color: white;
+    margin-bottom: 20px;
+`
+
+const Info = styled.p`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+    color: white;
+    font-weight: lighter;
+`
+
+const InfoHighlight = styled.span`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+    color: white;
+    font-weight: bold;
 `
